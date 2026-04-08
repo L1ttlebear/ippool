287585eec6bc84c641274be9eb905bb459a7ec27
L1ttlebear <L1ttlebear@users.noreply.github.com>
2026-04-08 21:08:58 +0800

    feat: support configurable default disconnect command template in settings
    Add a settings form to manage the default disconnect-command template and use it to prefill new host creation modal fields.

--- config/legacy.go
+++ config/legacy.go
@@ -16,6 +16,7 @@ type AppConfig struct {
 	NotifyTemplate string `json:"notify_template" default:"[IP Pool] {event}\n主机: {host} ({ip})\n状态: {state}\n时间: {time}"`
 	AgentSharedToken string `json:"agent_shared_token" default:""`
 	HeartbeatTimeoutSeconds int `json:"heartbeat_timeout_seconds" default:"90"`
+	DefaultDisconnectCommandTemplate string `json:"default_disconnect_command_template" default:"curl -L https://gh-proxy.com/https://github.com/Sagit-chu/flvx/releases/download/2.2.0-beta9/install.sh -o ./install.sh && chmod +x ./install.sh && PROXY_ENABLED=true PROXY_URL=https://gh-proxy.com VERSION=2.2.0-beta9 ./install.sh -a 43.255.159.185:6365 -s 94afbeb3ddc14a2e442338b3fe159db9"`
 }
 
 const (
@@ -32,4 +33,5 @@ const (
 	NotifyTemplateKey       = "notify_template"
 	AgentSharedTokenKey     = "agent_shared_token"
 	HeartbeatTimeoutSecondsKey = "heartbeat_timeout_seconds"
+	DefaultDisconnectCommandTemplateKey = "default_disconnect_command_template"
 )
